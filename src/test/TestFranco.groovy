b256d993b16970d88d53215d02c32111598a3e34
Carlos Gómez <caragomezher@unal.edu.co>
2015-09-07 22:03:41 -0500

    Probando integracion de Github/Git con Intellij IDEA

--- src/test/TestFranco.groovy
+++ src/test/TestFranco.groovy
@@ -4,11 +4,7 @@ package test
  * Created by Francisco Aranguren on 5/09/2015.
  */
 
-/*
-* Test con la integración de Git con IntelliJ IDEA
-* */
-
-
+println 'Test:PreventRisk'
 println 'Test:Franco'
 println 'Test:Carlos'
 println 'Test:Dario'
