880a8b42fee00ef7fdbf873b7577b098b224d90c
frankz93 <franto93@yahoo.es>
2015-10-10 19:34:07 -0500

    Re-created project
    After a lot of struggle, the groovy project is now a grails project

--- src/test/TestFranco.groovy
+++ src/test/TestFranco.groovy
@@ -4,9 +4,4 @@ package test
  * Created by Francisco Aranguren on 5/09/2015.
  */
 
-println 'Test:PreventRisk'
 println 'Test:Franco'
-println 'Test:Carlos'
-println 'Test:Dario'
-println 'Test:Monica'
-println 'Test:Nicolas'
