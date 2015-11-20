09302d6484930c5ab1e37a2efc09c9580e7796c2
nordonezc <nordonezc@unal.edu.co>
2015-11-19 22:17:55 -0500

    Revert "Adding parameters to domain Class"
    This reverts commit 53b9c4d765ce095ab4398166a247fa206f91f4e4.

--- grails-app/domain/Emergency.groovy
+++ grails-app/domain/Emergency.groovy
@@ -4,13 +4,13 @@ class Emergency {
 
     static hasMany = [symptoms:Symptom]
     String name
-    String category
     String steps
+
 
     static constraints = {
         name nullable: false
-        steps nullable: false, maxSize: 2000
+        steps nullable: false
         symptoms nullable: false
-        category nullable: false
+
     }
 }
--- grails-app/domain/Symptom.groovy
+++ grails-app/domain/Symptom.groovy
@@ -1,16 +1,13 @@
-import javax.swing.Icon
+
 
 class Symptom {
     static hasMany = [emergencies:Emergency]
     static belongsTo = [Emergency]
     String name
-    String question
-    String img
 
     static constraints = {
         name nullable: false
         emergencies nullable: false
-        question nullable: false
 
     }
 }
