6867c0c4bba2e41823e0fd9b8af239a1c95d9f84
frankz93 <franto93@yahoo.es>
2015-11-29 08:19:01 -0500

    Simulator working
    Now the simulator do the stuffs almost good. (With a few exceptions)

--- grails-app/controllers/EmergencyController.groovy
+++ grails-app/controllers/EmergencyController.groovy
@@ -9,6 +9,7 @@ class EmergencyController {
     def emergencySelected = []
     def emergency_symptoms = [1:[1,2,3,4,5,6,7,8,9,12], 2:[13,14,16,17,18,34,20,19,15], 3:[21,24,34], 4:[21,26], 5:[5,14,28,30,9,12,31], 6:[34], 7:[16,37,6,38,20,24,9], 8:[39,23,40,41,42], 9:[7,13,16,20,43,44], 10:[50,47], 11:[50,46], 12:[50,48], 13:[], 14:[16,40,41,39,50,51], 15:[7,17,18,21,34,20,10,52], 16:[40,31,41,56,55], 17:[57], 18:[28,8,41,55,53,63,12,58,59,61,62], 19:[40,63]]
     def matrixEmergency = new int[20][64]
+    def idMayorSymptom = 0
 
 
     def index(Integer max) {
@@ -42,30 +43,27 @@ class EmergencyController {
                     if(emergency_symptoms[emergencias].contains(sintomas)) {
                         matrixEmergency[emergencias][sintomas] = 1
                     }
-                }
-                else
+                }else{
                     matrixEmergency[emergencias][sintomas] = 0
+                }
             }
         }
         println "Matriz inicial-------------"
         for(int i = 0; i<19; i++) println matrixEmergency[i]
-        redirect(action: 'question', id: "")
+        redirect(action: 'find_question')
     }
 
     def emergencyType() {
 
     }
 
-    def first_question(){
-
-    }
-
-    def question(String id){
+    def find_question(){
+        boolean justOne = false
+        boolean all = false
+        int count_zeros = 0
+        int emergencyRta = 0
         def mayorSymptomActual = 0
         def mayorSymptom = 0
-        def idMayorSymptom = 0
-        def choosen = 0
-
 
         for(int sintomas=0; sintomas<=63; sintomas++){
             for(int emergencias=0; emergencias<=19; emergencias++){
@@ -79,36 +77,12 @@ class EmergencyController {
             }
             mayorSymptom = 0
         }
+
         println idMayorSymptom
-
-        if (id == "YES"){
-            println "YES! YES! YES! YES!"
-            choosen = 0
-        }
-
-        else if (id == "NO"){
-            println "NO! NO! NO! NO! NO!"
-            choosen = 1
-        }
-
-        for(int emergencias = 0; emergencias<=19; emergencias++){
-            if(matrixEmergency[emergencias][idMayorSymptom] == choosen){
-                for(int sintomas=0; sintomas<=63; sintomas++) matrixEmergency[emergencias][sintomas] = 0
-            }
-            matrixEmergency[emergencias][idMayorSymptom] = 0
-        }
-
-        for(int i = 0; i<=19; i++) println matrixEmergency[i]
-
-        boolean justOne = false
-        boolean all = false
-        int count_zeros = 0
-        int emergencyRta = 0
-
         for(int enfermedades = 0; enfermedades <=19; enfermedades++){
             if (matrixEmergency[enfermedades].sum() > 0){
                 emergencyRta = enfermedades
-                if(justOne == false) justOne = true
+                if(!justOne) justOne = true
                 else{
                     justOne = false
                     break
@@ -130,12 +104,35 @@ class EmergencyController {
         else if(!justOne) {
             if (all) render(view: "answer", model: [name: "Cancer", steps: ""])
             else {
-                println idMayorSymptom + " con numero de repeticiones " + mayorSymptomActual
                 def symptomRepet = Symptom.get(idMayorSymptom)
                 render(view: "question", model: [question: symptomRepet.question, imagen: symptomRepet.img])
             }
         }
+    }
 
+    def question(String id){
+        def choosen = -1
+
+        if (id == "YES"){
+            println "YES! YES! YES! YES!"
+            choosen = 0
+        }
+
+        else if (id == "NO"){
+            println "NO! NO! NO! NO! NO!"
+            choosen = 1
+        }
+
+        for(int emergencias = 0; emergencias<=19; emergencias++){
+            if(matrixEmergency[emergencias][idMayorSymptom] == choosen){
+                for(int sintomas=0; sintomas<=63; sintomas++) matrixEmergency[emergencias][sintomas] = 0
+            }
+            matrixEmergency[emergencias][idMayorSymptom] = 0
+        }
+
+        for(int i = 0; i<=19; i++) println matrixEmergency[i]
+
+        redirect(action: "find_question")
     }
 
 
